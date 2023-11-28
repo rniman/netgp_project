d84156b67994680204b9c912263e76ba9648bbdb
yunbem <yunbeomgo@gmail.com>
2023-11-28 14:06:32 +0900

    머지 전 커밋

--- 3반-14팀 드래곤 고로시/Server/MainCharacter.cpp
+++ 3반-14팀 드래곤 고로시/Server/MainCharacter.cpp
@@ -3,7 +3,6 @@
 
 void CreateMainChar(/*HINSTANCE g_hInst,*/ MainCharacter* mainCharacter)
 {
-
 	mainCharacter->info.state = MainState::IDLE;
 	mainCharacter->info.animationNum = 0;
 	mainCharacter->info.right = FALSE;
@@ -12,89 +11,17 @@ void CreateMainChar(/*HINSTANCE g_hInst,*/ MainCharacter* mainCharacter)
 
 	mainCharacter->info.heart = 6;
 
-	// BITDATA를 CLIENT에서 넘겨받는 부분을 추가해서 여기서 초기화 해줘야함
-	// 
-	//TCHAR temp[50];
-	//BITMAP bmData;
-	//for (int i = 0; i < IDLEANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/idle/cuphead_idle_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.IDLEBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.IDLEBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.IDLEBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.IDLEBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < RUNANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/run/cuphead_run_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.RUNBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.RUNBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.RUNBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.RUNBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < JUMPANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/jump/cuphead_jump_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.JUMPBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.JUMPBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.JUMPBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.JUMPBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < SHOOTANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/shoot/cuphead_shoot_straight_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.SHOOTBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.SHOOTBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.SHOOTBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.SHOOTBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < RUNSHOOTANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/runshoot/cuphead_run_shoot_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.RUNSHOOTBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.RUNSHOOTBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.RUNSHOOTBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.RUNSHOOTBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < EXSHOOTANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/exshoot/cuphead_ex_straight_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.EXSHOOTBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.EXSHOOTBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.EXSHOOTBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.EXSHOOTBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < HITANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/hit/cuphead_hit_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.HITBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.HITBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.HITBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.HITBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < GHOSTANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/ghost/cuphead_ghost_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.GHOSTBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.GHOSTBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.GHOSTBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.GHOSTBitData[i].bmHeight = bmData.bmHeight;
-	//}
-	//for (int i = 0; i < REVIVEANI; ++i)
-	//{
-	//	wsprintf(temp, L"CUPHEAD/revive/cuphead_revive_00%02d.bmp", i + 1);
-	//	mainCharacter->bitmap.REVIVEBITMAP[i] = (HBITMAP)LoadImage(g_hInst, temp, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-	//	GetObject(mainCharacter->bitmap.REVIVEBITMAP[i], sizeof(BITMAP), &bmData);
-	//	mainCharacter->bitmap.REVIVEBitData[i].bmWidth = bmData.bmWidth;
-	//	mainCharacter->bitmap.REVIVEBitData[i].bmHeight = bmData.bmHeight;
-	//}
+	mainCharacter->info.coolTime = 0;
+	mainCharacter->info.invincibleTime = 0;
+	mainCharacter->info.jumpTime = 0;
+	mainCharacter->info.responeTime = 0;
+
+	//mainCharacter->info.type
 
 	mainCharacter->info.Pos.left = 0;
 	mainCharacter->info.Pos.right = 49;
-	//mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmWidth / 2;
 	mainCharacter->info.Pos.bottom = STAGEFLOOR;
 	mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - 77;
-	//mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmHeight / 2;
 
 	mainCharacter->info.energy = -1;
 
@@ -107,5 +34,155 @@ void CreateMainChar(/*HINSTANCE g_hInst,*/ MainCharacter* mainCharacter)
 		mainCharacter->info.bullet[i].direction = true;
 		//mainCharacter->info.bullet[i].hitBox = false;
 		//mainCharacter->info.bullet[i].Pos = false;
+	}
+}
+
+void InitMainChar(MainCharacter* mainCharacter)
+{
+	mainCharacter->info.state = MainState::IDLE;
+	mainCharacter->info.animationNum = 0;
+	mainCharacter->info.right = FALSE;
+	mainCharacter->info.left = FALSE;
+	mainCharacter->info.direction = TRUE;
+
+	mainCharacter->info.Pos.left = 0;
+	mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmWidth / 2;
+	mainCharacter->info.Pos.bottom = STAGEFLOOR;
+	mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmHeight / 2;
+
+	mainCharacter->info.energy = -1;
+}
+
+void MoveMainChar(MainCharacter* mainCharacter, RECT rect)
+{
+	if (mainCharacter->info.right)
+	{
+		mainCharacter->info.Pos.left += 10;
+		mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+
+		if (mainCharacter->info.Pos.right >= rect.right)
+		{
+			mainCharacter->info.Pos.right = rect.right;
+			mainCharacter->info.Pos.left = rect.right - mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		}
+	}
+	else if (mainCharacter->info.left)
+	{
+		mainCharacter->info.Pos.right -= 10;
+		mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		if (mainCharacter->info.Pos.left <= rect.left)
+		{
+			mainCharacter->info.Pos.left = rect.left;
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		}
+	}
+}
+
+void JumpMainChar(MainCharacter* mainCharacter, RECT rect)
+{
+	if (mainCharacter->info.jumpTime < 20)
+	{
+		mainCharacter->info.Pos.top -= 10;
+		mainCharacter->info.Pos.bottom -= 10;
+	}
+	else
+	{
+		mainCharacter->info.Pos.top += 10;
+		mainCharacter->info.Pos.bottom += 10;
+	}
+
+	if (mainCharacter->info.right)
+	{
+		mainCharacter->info.Pos.left += 10;
+		mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+
+		if (mainCharacter->info.Pos.right >= rect.right)
+		{
+			mainCharacter->info.Pos.right = rect.right;
+			mainCharacter->info.Pos.left = rect.right - mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		}
+	}
+	else if (mainCharacter->info.left)
+	{
+		mainCharacter->info.Pos.right -= 10;
+		mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+
+		if (mainCharacter->info.Pos.left <= rect.left)
+		{
+			mainCharacter->info.Pos.left = rect.left;
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		}
+	}
+}
+
+void ImpactMainChar(MainCharacter* mainCharacter, int* stage)
+{
+	if (mainCharacter->info.heart > 0)
+	{
+		mainCharacter->info.heart--;
+	}
+	else //게임오버
+	{
+		*stage = 0;
+	}
+
+	mainCharacter->info.state = MainState::HIT;
+}
+
+void HitBoxMainChar(MainCharacter* mainCharacter)
+{
+	switch (mainCharacter->info.state)
+	{
+	case MainState::IDLE:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.IDLEBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::RUN:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.RUNBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::JUMP:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.JUMPBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::SHOOT:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.SHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.SHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.SHOOTBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::RUNSHOOT:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.RUNSHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.RUNSHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.RUNSHOOTBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::EXSHOOT:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.EXSHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.EXSHOOTBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.EXSHOOTBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	case MainState::HIT:
+		if (mainCharacter->info.direction)
+			mainCharacter->info.Pos.right = mainCharacter->info.Pos.left + mainCharacter->bitmap.HITBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		else
+			mainCharacter->info.Pos.left = mainCharacter->info.Pos.right - mainCharacter->bitmap.HITBitData[mainCharacter->info.animationNum].bmWidth / 2;
+		mainCharacter->info.Pos.top = mainCharacter->info.Pos.bottom - mainCharacter->bitmap.HITBitData[mainCharacter->info.animationNum].bmHeight / 2;
+		break;
+	default:
+		break;
 	}
 }
--- 3반-14팀 드래곤 고로시/Server/server.cpp
+++ 3반-14팀 드래곤 고로시/Server/server.cpp
@@ -3,17 +3,35 @@
 #define SERVERPORT 9000
 #define KEYBUFSIZE    8
 
-MainCharacter p1, p2;
-BossMonster bossMob;
+HWND hWnd;
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = L"Window Class Name";
+LPCTSTR lpszWindowName = L"Window Programming Lab";
+HANDLE hNetworkThread;
+
+extern MainCharacter mainCharacter, p2;
+extern BossMonster bossMob;
+extern BossBitData bossBitData;
 
 ThreadParams p1ThreadParams, p2ThreadParams;
 // THREAD HANDLE
-HANDLE hP1Thread, hP2Thread;
+extern HANDLE hP1Thread, hP2Thread;
 HANDLE hUpdateThread;
 
 // EVENT HANDLE
-HANDLE hPlayer1Input, hPlayer2Input;
-HANDLE hPlayer1Update, hPlayer2Update;
+extern HANDLE hPlayer1Input, hPlayer2Input;
+extern HANDLE hPlayer1Update, hPlayer2Update;
+extern HANDLE hMainUpdate;
+
+//메인 캐릭터 및 총알
+extern BulletBitmap bulletBitmap;
+MainState oldState;
+int oldAnimationNum;
+
+//보스
+extern BossMonster Boss;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); //윈도우 프로시저 프로토선언 
 
 void processPlayerInput(SOCKET client_sock, const char* keyBuffer)
 {
@@ -21,141 +39,13 @@ void processPlayerInput(SOCKET client_sock, const char* keyBuffer)
 	send(client_sock, keyBuffer, KEYBUFSIZE, 0);
 }
 
-DWORD WINAPI NetworkThread(LPVOID arg)
-{
-	ThreadParams threadParams = *(ThreadParams*)arg;
-
-	int retval;
-	SOCKET client_sock = threadParams.socket;
-	struct sockaddr_in clientaddr;
-	char addr[INET_ADDRSTRLEN];
-	int addrlen;
-
-	// 클라이언트 정보 얻기
-	addrlen = sizeof(clientaddr);
-	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
-	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
-	printf("[클라이언트 접속 IP: %s, 포트 번호: %d]", addr, ntohs(clientaddr.sin_port));
-
-	// BITMAP WIDTH, HEIGHT값 수신
-	// tbd
-
-	// 초기화 작업
-	// tbd
-	CreateMainChar(&p1);
-
-	// INIT 데이터를 송신
-	// tbd
-
-	int len;
-	char buf[256];
-
-	while (1)
-	{
-		// INPUT 데이터를 받는다
-		// 임시로 빈 버퍼를 받는다.
-		/*retval = recv(client_sock, (char*)&len, sizeof(int), MSG_WAITALL);
-		if (retval == SOCKET_ERROR) 
-		{
-			err_display("recv()");
-			break;
-		}
-		else if (retval == 0)
-			break;*/
-
-		/*retval = recv(client_sock, (char*)buf, len, MSG_WAITALL);
-		if (retval == SOCKET_ERROR) 
-		{
-			err_display("recv()");
-			break;
-		}
-		else if (retval == 0)
-			break;*/
-
-		retval = recv(client_sock, (char*)buf, KEYBUFSIZE, MSG_WAITALL);
-		if (retval == SOCKET_ERROR)
-		{
-			err_display("recv()");
-			break;
-		}
-		else if (retval == 0)
-			break;
-
-		// 받은 데이터 출력
-		buf[retval] = '\0';
-		printf("[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
-
-		// 받은 데이터 처리
-		processPlayerInput(client_sock, buf);
-
-		if(hP1Thread == threadParams.hThread)
-		{
-			SetEvent(hPlayer1Input);
-		}
-		//else if(hP2Thread == threadParams.hThread)
-		//	SetEvent(hPlayer2Input);
-
-
-		// 업데이트 스레드가 완료되기를 기다린다.
-		if (hP1Thread == threadParams.hThread)
-		{
-			WaitForSingleObject(hPlayer1Update, INFINITE);
-		}
-		//else if (hP2Thread == threadParams.hThread)
-		//	WaitForSingleObject(hPlayer2Update, INFINITE);
-
-		SendUpdateData updateData;
-		updateData.player1 = p1.info;
-		//updateData.player2 = p2.info;
-
-		//// 서버에서 업데이트한 내용을 보내준다
-		//if (SendDefaultData(client_sock, updateData) == -1)
-		//{
-		//	// 오류 처리
-		//	printf("Send Default Error\n");
-		//}
-	}
-
-	// 소켓 닫기
-	closesocket(client_sock);
-	printf("[클라이언트 종료 IP : %s, 포트 번호 : %d]", addr, ntohs(clientaddr.sin_port));
-	
-	return 0;
-}
-
-DWORD WINAPI UpdateThread(LPVOID arg)
-{
-	while (1)
-	{	
-		// INPUT이 완료되기를 기다린다.
-		WaitForSingleObject(hPlayer1Input, INFINITE);
-		//WaitForSingleObject(hPlayer2Input, INFINITE);
-
-		//printf("업데이트를 수행합니다!!!!");
-		//Sleep(5000);
-		p1.info.animationNum++;
-		if (p1.info.state == MainState::IDLE)
-		{
-			p1.info.animationNum++;
-			if (p1.info.animationNum > 4)
-			{
-				p1.info.animationNum = 0;
-			}
-		}
-
-		// 업데이트 완료
-		SetEvent(hPlayer1Update);
-		//SetEvent(hPlayer2Update);
-	}
-	return 0;
-}
-
-int main(int argc, char* argv[])
+DWORD WINAPI ServerMain(LPVOID arg)
 {
 	int retval;
 
 	hPlayer1Input = CreateEvent(NULL, FALSE, FALSE, NULL);
 	hPlayer1Update = CreateEvent(NULL, FALSE, FALSE, NULL);
+	hMainUpdate = CreateEvent(NULL, FALSE, FALSE, NULL);
 
 	hUpdateThread = CreateThread(NULL, 0, UpdateThread, NULL, 0, NULL);
 
@@ -186,11 +76,11 @@ int main(int argc, char* argv[])
 	int addrlen;
 
 	int nPlayer = 1;
-	while (1) 
+	while (1)
 	{
 		// 일단은 p1부터 받아보자
 		addrlen = sizeof(clientaddr);
-		if(nPlayer == 1)
+		if (nPlayer == 1)
 		{
 			// accept()
 			p1ThreadParams.socket = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
@@ -199,6 +89,11 @@ int main(int argc, char* argv[])
 				err_display("accept()");
 				break;
 			}
+			srand(time(NULL));
+			SetTimer(hWnd, 1, 15, NULL);
+			SetTimer(hWnd, 5, 90, NULL);
+			SetTimer(hWnd, 10, 100, NULL);
+
 		}
 		else if (nPlayer == 2)
 		{
@@ -229,11 +124,358 @@ int main(int argc, char* argv[])
 				closesocket(p2ThreadParams.socket);
 			}
 		}
-
 		++nPlayer;
 	}
 
 	closesocket(listen_sock);	// 소켓 닫기
 	WSACleanup();				// 윈속 종료
 	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	MSG Message;			//윈도우 메시지 선언
+	WNDCLASSEX WndClass;	//윈도우 클래스 선언 ->생성하느 윈도우의 형태를 설정하기위한 구조체
+	g_hInst = hInstance;
+
+	WndClass.cbSize = sizeof(WndClass);								//본 구조체의 크기
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;						//출력 스타일
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;						//프로시저 함수의 이름
+	WndClass.cbClsExtra = 0;										//클래스 여분 메모리--사용은 안함
+	WndClass.cbWndExtra = 0;										//윈도우 여분 메모리--마찬가지로 사용은 안함
+	WndClass.hInstance = hInstance;									//윈도우 인스턴스 ->이 윈도우 클래스를 사용하는 프로그램의 인스터스 값,WinMain함수의 인수로 전달된 hInstance 값 사용
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);				//아이콘(작업창 아이콘 바뀜)
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);					//커서(커서 바뀜)
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);	//배경색
+	WndClass.lpszMenuName = NULL;									//메뉴이름
+	WndClass.lpszClassName = lpszClass;								//클래스 이름
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);				//작은 아이콘,캡션 아이콘
+	RegisterClassEx(&WndClass);										//생성한 윈도우 클래스를 운영체제에 등록한다.
+
+	hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 1000, 600, NULL, (HMENU)NULL, hInstance, NULL);
+
+	//// 윈속 초기화
+	//WSADATA wsa;
+	//if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	//	return 1;
+	// 이벤트 생성
+	// tbd
+
+	// 소켓 통신 스레드 생성
+	hNetworkThread = CreateThread(NULL, 0, ServerMain, NULL, 0, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Message, 0, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) //CALLBACK 함수는 WinMain에서 호출하는 것이 아닌 윈도우 OS가 호출한다.
+{
+	static RECT rect;
+	static BOOL potalOn = FALSE;
+
+	static int victoryNum = 0;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		GetClientRect(hWnd, &rect);
+
+		break;
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case 1:
+			//방향키 확인
+			if (GetKeyState(VK_RIGHT) >= 0 && mainCharacter.info.right)
+			{
+				mainCharacter.info.right = FALSE;
+			}
+			if (GetKeyState(VK_LEFT) >= 0 && mainCharacter.info.left)
+			{
+				mainCharacter.info.left = FALSE;
+			}
+
+			HitBoxMainChar(&mainCharacter);
+
+			//쿨타임 
+			if (mainCharacter.info.coolTime > 0)mainCharacter.info.coolTime--;
+			else mainCharacter.info.coolTime = 0;
+
+			BossAttackMeteor(rect, &Boss, &mainCharacter, &oldState, &oldAnimationNum); //oldState 수정필요
+
+			if (mainCharacter.info.invincibleTime != 0)
+			{
+				mainCharacter.info.invincibleTime--;
+			}
+
+			if (mainCharacter.info.heart <= 0)
+			{
+				if (mainCharacter.info.responeTime == 0)
+				{
+					mainCharacter.info.responeTime = 500;
+				}
+
+				if (mainCharacter.info.responeTime > 280)
+				{
+					mainCharacter.info.animationNum++;
+					if (mainCharacter.info.animationNum > 23)
+					{
+						mainCharacter.info.animationNum = 0;
+					}
+					mainCharacter.info.responeTime--;
+				}
+				else
+				{
+					if (mainCharacter.info.responeTime == 280)
+					{
+						mainCharacter.info.animationNum = 0;
+					}
+					else if (mainCharacter.info.responeTime % 10 == 0)
+					{
+						mainCharacter.info.animationNum++;
+					}
+					mainCharacter.info.responeTime -= 5;
+				}
+
+
+				if (mainCharacter.info.responeTime == 0)
+				{
+					mainCharacter.info.heart = 6;
+
+					if (oldState != MainState::JUMP) mainCharacter.info.state = MainState::IDLE;
+					else mainCharacter.info.state = MainState::JUMP;
+
+					mainCharacter.info.animationNum = 0;
+					mainCharacter.info.right = FALSE;
+					mainCharacter.info.left = FALSE;
+					mainCharacter.info.direction = TRUE;
+					mainCharacter.info.energy = -1;
+					mainCharacter.info.invincibleTime = 100;
+				}
+			}
+			else if (mainCharacter.info.state == MainState::HIT)
+			{
+				if (mainCharacter.info.invincibleTime % 10 == 0)
+					mainCharacter.info.animationNum++;
+
+				if (mainCharacter.info.animationNum == 5)
+				{
+					if (oldState == MainState::EXSHOOT)
+						mainCharacter.info.state = MainState::IDLE;
+					else
+						mainCharacter.info.state = oldState;
+					mainCharacter.info.animationNum = oldAnimationNum;
+				}
+			}
+			else if (mainCharacter.info.state == MainState::JUMP)
+			{	//점프
+				mainCharacter.info.animationNum++;
+				JumpMainChar(&mainCharacter, rect);
+				if (GetKeyState(VK_CONTROL) < 0 && mainCharacter.info.coolTime == 0)
+				{
+					CreateBullet(mainCharacter, bulletBitmap);
+					mainCharacter.info.coolTime = 18;
+				}
+
+				if (mainCharacter.info.animationNum > 7)
+				{
+					mainCharacter.info.animationNum = 0;
+				}
+				mainCharacter.info.jumpTime++;
+
+				//점프 후 낙하시 발판이 있으면 발판에 찾기
+				if (mainCharacter.info.jumpTime >= 40)
+				{
+					mainCharacter.info.state = MainState::IDLE;
+					mainCharacter.info.jumpTime = 0;
+				}
+			}
+			else if (mainCharacter.info.state == MainState::EXSHOOT)
+			{
+				mainCharacter.info.energy++;
+				if (mainCharacter.info.energy == 0)mainCharacter.info.animationNum = 0;
+				else if (mainCharacter.info.energy % 5 == 0 && mainCharacter.info.animationNum < 4)mainCharacter.info.animationNum++;
+
+				if (GetKeyState(VK_SHIFT) >= 0)
+				{
+					if (mainCharacter.info.animationNum < 4)
+					{
+						mainCharacter.info.state = MainState::IDLE;
+						mainCharacter.info.energy = -1;
+					}
+					else if (mainCharacter.info.animationNum == 4)
+					{
+						CreateBullet(mainCharacter, bulletBitmap);
+						mainCharacter.info.animationNum++;
+					}
+				}
+
+				if (mainCharacter.info.energy % 5 == 0 && mainCharacter.info.animationNum < EXSHOOTANI - 1 && mainCharacter.info.animationNum > 4)
+				{
+					mainCharacter.info.animationNum++;
+				}
+				else if (mainCharacter.info.animationNum == EXSHOOTANI - 1)
+				{
+					mainCharacter.info.state = MainState::IDLE;
+					mainCharacter.info.energy = -1;
+				}
+			}
+			else if (mainCharacter.info.left && mainCharacter.info.right)
+			{
+				if (GetKeyState(VK_CONTROL) >= 0)
+				{
+					mainCharacter.info.state = MainState::IDLE;
+				}
+				else
+				{
+					mainCharacter.info.state = MainState::SHOOT;
+				}
+
+				mainCharacter.info.Pos.right = mainCharacter.info.Pos.left + IDLEWIDTH;
+			}
+			else if (mainCharacter.info.right || mainCharacter.info.left)
+			{
+				mainCharacter.info.animationNum++;
+				if (GetKeyState(VK_CONTROL) >= 0)
+				{
+					mainCharacter.info.state = MainState::RUN;
+				}
+				else
+				{
+					if (mainCharacter.info.coolTime == 0)
+					{
+						CreateBullet(mainCharacter, bulletBitmap);
+						mainCharacter.info.coolTime = 18;
+					}
+					mainCharacter.info.state = MainState::RUNSHOOT;
+				}
+
+				MoveMainChar(&mainCharacter, rect);
+				if (mainCharacter.info.animationNum > 15)
+				{
+					mainCharacter.info.animationNum = 0;
+				}
+			}
+
+			//상태 변환
+			if (mainCharacter.info.state == MainState::HIT || mainCharacter.info.heart <= 0) {}
+			else if (GetKeyState(VK_RIGHT) < 0)mainCharacter.info.direction = TRUE;
+			else if (GetKeyState(VK_LEFT) < 0)mainCharacter.info.direction = FALSE;
+			else if (GetKeyState(VK_CONTROL) < 0 && mainCharacter.info.state != MainState::JUMP) mainCharacter.info.state = MainState::SHOOT;
+			else if (mainCharacter.info.state != MainState::JUMP && mainCharacter.info.state != MainState::EXSHOOT) mainCharacter.info.state = MainState::IDLE;
+
+			MoveBullet(mainCharacter, rect);
+			DeathBullet(mainCharacter);
+
+			// 보스 공격 사각형 설정
+			SetBossAndBossAttackRect(Boss, bossBitData);
+			// 이제 이벤트를 발생시키자
+			SetEvent(hMainUpdate);
+
+			//InvalidateRect(hWnd, NULL, FALSE);
+			break;
+		case 2:		//꼬리 공격 준비 할 텀 타임
+			//그냥 0.01초 쉬는 용도인듯?
+			SetTimer(hWnd, 3, 10, NULL);
+			//Boss.AttackTailReady = TRUE;
+			KillTimer(hWnd, 2);
+			break;
+		case 3:		//꼬리 공격 준비 타임
+			Boss.AttackTailReady = TRUE;
+			BossAttackTail(hWnd, rect, &mainCharacter, &Boss, &oldState, &oldAnimationNum); //보스 꼬리 공격 구조 + oldState바꾸기
+			break;
+		case 5:
+			//IDLE 및 IDLE SHOOT 상태
+			if (mainCharacter.info.state == MainState::IDLE)
+			{
+				mainCharacter.info.animationNum++;
+				if (mainCharacter.info.animationNum > 4)
+				{
+					mainCharacter.info.animationNum = 0;
+				}
+			}
+			else if (mainCharacter.info.state == MainState::SHOOT)
+			{
+				mainCharacter.info.animationNum++;
+
+				if (mainCharacter.info.animationNum == 1)
+				{
+					CreateBullet(mainCharacter, bulletBitmap);
+					mainCharacter.info.coolTime = 18;
+				}
+
+				if (mainCharacter.info.animationNum > 2)
+				{
+					mainCharacter.info.animationNum = 0;
+				}
+			}
+			break;
+		case 10:
+			BossStateChange(&Boss, hWnd, rect);
+			BossAttackStateChange(&Boss, rect);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_RIGHT:
+			mainCharacter.info.right = TRUE;
+			mainCharacter.info.direction = TRUE;
+			break;
+		case VK_LEFT:
+			mainCharacter.info.left = TRUE;
+			mainCharacter.info.direction = FALSE;
+			break;
+		case VK_SPACE:
+			if (mainCharacter.info.state != MainState::EXSHOOT && mainCharacter.info.state != MainState::HIT)
+			{
+				mainCharacter.info.state = MainState::JUMP;
+			}
+			break;
+		case VK_CONTROL:
+			if (mainCharacter.info.state != MainState::JUMP && mainCharacter.info.state != MainState::HIT)
+			{
+				if (mainCharacter.info.state == MainState::RUN)
+				{
+					mainCharacter.info.state = MainState::RUNSHOOT;
+				}
+				else if (mainCharacter.info.state == MainState::IDLE)
+				{
+					mainCharacter.info.state = MainState::SHOOT;
+				}
+			}
+			break;
+		case VK_SHIFT:
+			if (mainCharacter.info.state != MainState::JUMP && mainCharacter.info.state != MainState::HIT)
+			{
+				mainCharacter.info.state = MainState::EXSHOOT;
+			}
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		KillTimer(hWnd, 1);
+		KillTimer(hWnd, 2);
+		KillTimer(hWnd, 3);
+		KillTimer(hWnd, 5);
+		KillTimer(hWnd, 10);
+
+
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
 }
